12931d0889748a7da7cb19d3075b88f200af91e1
Jacinda Shelly <jshelly@doctorondemand.com>
2013-07-12 13:59:50 -0700

    Remove verify_exists keyword from URLFields which was deprecated in Django 1.5

--- djangopypi/forms.py
+++ djangopypi/forms.py
@@ -90,7 +90,7 @@ class Metadata10Form(forms.Form):
                                            'be used to assist searching for the '
                                            'package in a larger catalog'))
     
-    home_page = forms.URLField(required=False, verify_exists=True,
+    home_page = forms.URLField(required=False,
                                help_text=_(u'A string containing the URL for '
                                            'the package\'s home page.'))
     
@@ -126,7 +126,7 @@ class Metadata11Form(Metadata10Form):
                                            'be used to assist searching for the '
                                            'package in a larger catalog'))
     
-    download_url = forms.URLField(required=False, verify_exists=True,
+    download_url = forms.URLField(required=False,
                                   help_text=_(u'A string containing the URL for '
                                               'the package\'s home page.'))
     
@@ -170,7 +170,6 @@ class Metadata12Form(Metadata10Form):
                                            'package in a larger catalog'))
     
     download_url = forms.URLField(required=False,
-                                  verify_exists=True,
                                   help_text=_(u'A string containing the URL for '
                                               'the package\'s home page.'))
     
